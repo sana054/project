c4a65e0abc63a0441d6e23043e3c55919e5ef66b
sana054 <36442395+sana054@users.noreply.github.com>
2018-04-13 10:29:41 +0100

    comm2
    edit app module to import service

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,26 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { APP_BASE_HREF } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-
+import { CommonModule } from "@angular/common";
 import { AboutModule } from './about/about.module';
 import { HomeModule } from './home/home.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
+import {RequestsService} from "./requests.service";
+import {FormsModule} from "@angular/forms";
+
 
 
 @NgModule({
   imports: [BrowserModule, CoreModule,
-    HttpClientModule, AppRoutingModule,
+    HttpClientModule, AppRoutingModule, FormsModule,
     AboutModule, HomeModule,
     SharedModule.forRoot()],
   declarations: [AppComponent],
-  providers: [{
-    provide: APP_BASE_HREF,
-    useValue: '<%= APP_BASE %>'
-  }],
+  providers: [
+    RequestsService,CommonModule
+
+  ],
   bootstrap: [AppComponent]
 
 })
